0f6f06434509471a277be1b5e9056aa8063e6e0f
SemenZhilinskiy1999 <99233461+SemenZhilinskiy1999@users.noreply.github.com>
2023-01-11 22:32:07 +0700

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,32 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-
-namespace ConsoleApp64
+namespace Lab2Chart
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            double x0, xn, xnp1, e;
-          
-            e = 0.001;
-            x0 = 2;
-            xn = x0 - (Math.Pow(x0, 2)-2)/(2*x0) ;
-            xnp1 = xn - (Math.Pow(xn, 2)-2)/(2*xn);
-
-
-            while (xn - xnp1 >= e)
-            {
-                xn = xnp1;
-                xnp1 = xn - (Math.Pow(xn, 2)-2) / (2 * xn);
- 
-            }
-
-            Console.WriteLine("{0:0.000}", xnp1);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
